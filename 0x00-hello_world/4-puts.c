5c034b97af6d3db71db3d1f48923143f683dc88b
Emran <ouakouriimran@gmail.com>
2023-07-13 20:07:31 +0100

    betty confusion

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,11 @@
-#!/bin/bash
 #include <stdio.h>
+/**
+ *main - printing code
+ *
+ * Return: Always 0 (success)
+*/
 int main(void)
-{	
+{
 	puts("\"Programming is like building a multilingual puzzle");
-	return(0)
-}		
+	return (0);
+}
