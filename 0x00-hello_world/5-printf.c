e4f0d0872d1148efb6e477d4f41c27dd66638fbf
Emran <ouakouriimran@gmail.com>
2023-07-13 20:18:27 +0100

    fury

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ *  *main - printing code
+ *   *
+ *    * Return: Always 0 (success)
+ *    */
+int main(void)
+{
+	        printf("\"Programming is like building a multilingual puzzle");
+		        return (0);
+}
