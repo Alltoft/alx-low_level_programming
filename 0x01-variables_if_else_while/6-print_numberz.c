f4450467c5d3115c591cc429f4cebc9152127741
Emran <ouakouriimran@gmail.com>
2023-07-14 22:49:06 +0100

    hahah funny

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - printing numbers
+ *
+ * Return: 0
+ */
+
+int main(void)
+
+{
+	char a = '0';
+
+	for (a = '0'; a <= '9';)
+	{
+	putchar(a);
+	a++;
+	}
+	putchar('\n');
+	return (0);
+}
