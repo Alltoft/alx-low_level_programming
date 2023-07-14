da882e7b5ded61050c303888172d3e8a1bd25f50
Emran <ouakouriimran@gmail.com>
2023-07-14 21:14:40 +0100

    alphabet game

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Alphabet game
+ *
+ * Return: (0)
+ */
+
+int main(void)
+{
+	int x = a
+
+	while (x <= z)
+	{
+	putchar("%d", x)
+	x++
+	}
+	putchar("\n")
+	return (0)
+}
