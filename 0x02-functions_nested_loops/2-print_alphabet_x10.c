3b08471fbe4fd1e2f7406fb0f70ae842c87b82b3
Emran <ouakouriimran@gmail.com>
2023-07-18 22:38:57 +0100

    wahya

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -10,15 +10,15 @@ void print_alphabet_x10(void)
 	char x = 'a';
 	char j = 0;
 
-	while (j <= 10)
+	while (j < 10)
 	{
 	while (x <= 'z')
 	{
 	_putchar(x);
 	x++;
 	}
+	x = 'a';
 	_putchar('\n');
 	j++;
-	return;
 	}
 }
