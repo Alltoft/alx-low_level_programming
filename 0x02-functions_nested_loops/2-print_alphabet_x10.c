abeefe0ec687a512b97980a1b5e2e67fb9a6f48b
Emran <ouakouriimran@gmail.com>
2023-07-18 23:46:42 +0100

    finalllyyyyyyyyyyyyyyyyy

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,6 +1,8 @@
 #include "main.h"
-/*
- * print_alphabet_x10 - Prints the English alphabet 10 times.
+/**
+ * print_alphabet_x10 - print alphabet 10x  in lower case, followed by new line
+ *
+ * Return: Always 0.
  */
 void print_alphabet_x10(void)
 
