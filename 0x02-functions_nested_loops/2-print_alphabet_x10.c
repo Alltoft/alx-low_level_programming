f26964fef4229c1c0fa2a7744d7008e15df1dc52
Emran <ouakouriimran@gmail.com>
2023-07-18 22:06:56 +0100

    wahya

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include "main.h"
+ /**
+  * print_alphabet_x10 - print ten times
+  *
+  * Return: void
+  */
+
+void print_alphabet_x10(void)
+{
+	char x = 'a';
+	char j = '\n';
+	while (j <= 10)
+	{
+	while (x <= 'z')
+	{
+	_putchar('x');
+	x++;
+	}
+	_putchar('\n');
+	}
+}
