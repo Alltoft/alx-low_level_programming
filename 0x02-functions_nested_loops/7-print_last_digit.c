1fa5aa633eaed4a2043971af8ece3bf6f0d979d9
Emran <ouakouriimran@gmail.com>
2023-07-19 01:44:29 +0100

    last digit

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * print_last_digit - prints the last difit of the given number
+ * @c: is the variable that represents the number
+ * Return: the value of the last digit x
+ */
+int print_last_digit(int c)
+{
+	int x;
+
+	x = c % 10;
+	if (x < 0)
+	{
+	x = x * (-1);
+	}
+	_putchar(x);
+	return (x);
+}
