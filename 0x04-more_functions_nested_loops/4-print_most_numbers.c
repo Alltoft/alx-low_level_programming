452bef96596e1a4773b89359ffb5af1762161cb0
Emran <ouakouriimran@gmail.com>
2023-07-20 18:44:32 +0100

    printing numbers

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_most_numbers - it prints numbers from 0 to 9
+ */
+void print_most_numbers(void)
+{
+	int a = '0';
+
+	while (a <= '9')
+	{
+	if (a != '2' || a != '4')
+	_putchar(a);
+	a++;
+	}
+	_putchar('\n');
+}
