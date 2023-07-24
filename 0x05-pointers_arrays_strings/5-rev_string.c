52cfb73d0b3df75da325a5174a317c2f089ce16a
Emran <ouakouriimran@gmail.com>
2023-07-24 21:29:32 +0100

    reverse again

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * print_rev - print the revers of an string
+ * @s: the pointer of the string
+ * Return: nothing
+ */
+void print_rev(char *s)
+{
+	int a = 0;
+
+	while (*s != '\0')
+	{
+	s++;
+	a++;
+	}
+	while (a > 0)
+	{
+	a--;
+	s--;
+	_putchar(*s);
+	}
+	_putchar('\n');
+}
