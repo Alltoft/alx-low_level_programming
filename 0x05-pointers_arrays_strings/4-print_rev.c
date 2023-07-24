1a7244acfea06b6c3e2540325ca376d49389a249
Emran <ouakouriimran@gmail.com>
2023-07-24 22:41:17 +0100

    prev

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,10 +1,10 @@
 #include "main.h"
 /**
- * rev_string - print the revers of an string
+ * print_rev - print the revers of an string
  * @s: the pointer of the string
  * Return: nothing
  */
-void rev_string(char *s)
+void print_rev(char *s)
 {
 	int a = 0;
 
