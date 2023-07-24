2629f1ff1950846d82352b82547e70a833304570
Emran <ouakouriimran@gmail.com>
2023-07-24 19:14:18 +0100

    I fear computers

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _puts - it prints the string to stdout
+ * @str: the string that needs to be printed
+ * Return: nothing
+ */
+void _puts(char *str)
+{
+	while (*str != '\0')
+	{
+	_putchar(*str);
+	*str++;
+	}
+}
