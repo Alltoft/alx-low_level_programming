5c4b65de6677f7c4f36d18db5abb62c602ab0c59
Emran <ouakouriimran@gmail.com>
2023-07-27 19:51:01 +0100

    all up

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * *cap_string - changes lowercase to uppercase
+ * @ptr: the pointer to the string
+ * Return: the result (ptr)
+ */
+char *cap_string(char *ptr)
+{
+	char *origin = ptr;
+
+	while (*ptr != '\0')
+	{
+	if (*ptr == ' ' || *ptr == '\t' || *ptr == '\n' || *ptr == ',' ||
+	*ptr == ';' || *ptr == '.' || *ptr == '!' || *ptr == '?' ||
+	*ptr == '"' || *ptr == '(' || *ptr == ')' || *ptr == '{' ||
+	*ptr == '}')
+	{
+	ptr++;
+	if (*ptr >= 'a' && *ptr <= 'z')
+	{
+	*ptr = *ptr - 32;
+	}
+	}
+	ptr++;
+	}
+	return (origin);
+}
