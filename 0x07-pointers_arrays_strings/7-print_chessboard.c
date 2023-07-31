7b7dbe915b2fac798a52435bcef5a908ec55e3b9
Emran <ouakouriimran@gmail.com>
2023-08-01 00:32:27 +0100

    who wants to play

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -8,7 +8,7 @@ void print_chessboard(char (*a)[8])
 	int i = 0;
 	int j = 0;
 
-	while (i <= 8)
+	while (i < 8)
 	{
 		j = 0;
 		while (j < 8)
@@ -16,7 +16,7 @@ void print_chessboard(char (*a)[8])
 			_putchar(a[i][j]);
 			j++;
 		}
-		_putchar(a[i][j]);
+		_putchar('\n');
 		i++;
 	}
 }
