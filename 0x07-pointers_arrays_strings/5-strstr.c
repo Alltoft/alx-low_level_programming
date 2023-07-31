5883f8b67fbec2d6102d76f779eb6ed7269a3a68
Emran <ouakouriimran@gmail.com>
2023-07-31 23:05:31 +0100

    nna9ala

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,35 @@
+#include "main.h"
+/**
+ * *_strstr - locates a susbdtring
+ * @haystack: first string
+ * @needle: second string
+ * Return: 0
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	int i = 0;
+	int j = 0;
+
+	while (haystack[i] != '\0')
+	{
+	if (haystack[i] == ' ')
+	{
+		haystack++;
+		while (haystack[i] != ' ')
+		{
+			if (haystack[i] == needle[j])
+			{
+				return (haystack);
+			}
+			else
+			{
+				break;
+			}
+			haystack++;
+			needle++;
+		}
+	}
+	haystack++;
+	}
+	return (0);
+}
