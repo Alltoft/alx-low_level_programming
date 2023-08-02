3a26fd07026c10b86da74aabb9dfa9ccb3da812b
Emran <ouakouriimran@gmail.com>
2023-08-02 15:25:02 +0100

    print a string in another way

---
+++ 0x08_recursion.c/0-puts_recursion.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _puts_recursion - prints a string with recursion
+ * @s: the string that needs to be printed
+ */
+void _puts_recursion(char *s)
+{
+	if (*s != '\0')
+	{
+		_putchar(*s);
+		s++;
+		_puts_recursion(s);
+	}
+}
