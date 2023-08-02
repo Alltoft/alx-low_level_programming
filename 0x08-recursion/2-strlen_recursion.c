5546ee1756b0cc146e2e30b1c91bc8fe6862a130
Emran <ouakouriimran@gmail.com>
2023-08-02 18:07:24 +0100

    calculatrice

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _strlen_recursion - counts a string length
+ * @s: the string
+ * Return: the linght of the string
+ */
+int _strlen_recursion(char *s)
+{
+	int i = 0;
+
+	if (*s == '\0')
+	{
+		return (0);
+	}
+	else
+	{
+		return (1 + _strlen_recursion(s + 1));
+	}
+}
