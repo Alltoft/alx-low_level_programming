3d59bc361096cc3d8c5163b758a071d382c15489
Emran <ouakouriimran@gmail.com>
2023-08-04 16:35:10 +0100

    your name is

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - prints the name
+ *@argc: number of arguments
+ *@argv: the array
+ * Return: always 0
+ */
+int main(int argc, char *argv[])
+{
+	int i = 0;
+
+	if (i == 0)
+	{
+		printf("%s\n", argv[i]);
+	}
+	return (0);
+}
