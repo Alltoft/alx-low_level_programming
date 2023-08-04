1556bcd406213c387bb7243f85b566a760a0ccde
Emran <ouakouriimran@gmail.com>
2023-08-04 18:18:35 +0100

    darb

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -11,15 +11,15 @@ int main(int argc, char *argv[])
 	int i = 1;
 	int a;
 
-	if (argc == 1)
-	{
-		printf("Error\n");
-		return (1);
-	}
-	else if (argc == 3)
+	if (argc == 3)
 	{
 		a = atoi(argv[i]) * atoi(argv[i + 1]);
 		printf("%d\n", a);
+	}
+	else
+	{
+		 printf("Error\n");
+		 return (1);
 	}
 	return (0);
 }
