539bb0fdb9dfc1473f945dd9b0db17e0ca6227b7
Emran <ouakouriimran@gmail.com>
2023-08-08 20:55:50 +0100

    git commit

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -1,55 +1,41 @@
 #include <stdlib.h>
 #include "main.h"
+/**
+ * alloc_grid - Allocates memory for a
+ * two-dimensional array of integers
+ * @width: The width of the array
+ * @height: The height of the array
+ *
+ * Return: A pointer to the allocated memory
+ * or NULL if memory allocation
+ * fails or if width or height is 0 or negative
+ */
 int **alloc_grid(int width, int height)
 {
 int **ss;
 int n = 0;
 
 if (width <= 0 || height <= 0)
-    return (NULL);
+	return (NULL);
 
 ss = malloc(height * sizeof(int *));
 if (ss == NULL)
-    return (NULL);
+	return (NULL);
 
 while (n < height)
 {
-    ss[n] = malloc(width * sizeof(int));
-    if (ss[n] == NULL)
-    {
-        while (n > 0)
-        {
-            n--;
-            free(ss[n]);
-        }
-        free(ss);
-        return (NULL);
-    }
-    n++;
+	ss[n] = malloc(width * sizeof(int));
+	if (ss[n] == NULL)
+	{
+	while (n > 0)
+	{
+		n--;
+		free(ss[n]);
+	}
+	free(ss);
+	return (NULL);
+	}
+	n++;
 }
 return (ss);
 }
-/*int **alloc_grid(int width, int height)
-{
-	int **ss;
-	unsigned int n = 0;
-	unsigned int o = 0;
-
-	if (width <= 0 || height <= 0)
-		return (NULL);
-
-	ss = (int **) malloc(sizeof(char) * (width * height));
-	if (ss == NULL)
-		return (NULL);
-
-	while (n < height)
-	{
-		while (o < width)
-		{
-			o++;
-		}
-	o = 0;
-	n++;
-	}
-	return (ss);
-}*/
