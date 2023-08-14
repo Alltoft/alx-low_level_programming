7e1673ff917772945f078a6d83a8bb09914e8918
Emran <ouakouriimran@gmail.com>
2023-08-14 20:53:56 +0100

    fish smell

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -8,7 +8,7 @@
  * @age: dog age
  * @owner: dog owner
  *Return: new_dog
- */
+ *
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *new_dog;
@@ -36,4 +36,28 @@ dog_t *new_dog(char *name, float age, char *owner)
 		return (NULL);
 	}
 	return (new_dog);
+}*/
+dog_t *new_dog(char *name, float age, char *owner)
+{
+        dog_t *new_dog;
+
+        new_dog = malloc(sizeof(dog_t));
+        if (new_dog == NULL)
+                return (NULL);
+        new_dog->name = strdup(name);
+        if (new_dog->name == NULL)
+        {
+                free(new_dog);
+                return (NULL);
+        }
+        new_dog->age = age;
+        new_dog->owner = strdup(owner);
+        if (new_dog->owner == NULL)
+        {
+                free(new_dog->name);
+                free(new_dog);
+                return (NULL);
+        }
+        return (new_dog);
 }
+
