74307f0fed4d39fb196f1f96acb58f0d666dd6e6
Emran <ouakouriimran@gmail.com>
2023-08-14 21:42:16 +0100

    its haw haw not woof

--- 0x0E-structures_typedef/5-free_dog.c
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -1,15 +1,17 @@
 #include "dog.h"
 #include <stdlib.h>
 /**
- *free_dog - it frees dogs
- *@d: structer to free dogs
+ * free_dog - frees memory allocated to a dog structure
+ * @d: pointer to dog structure
+ *
+ * Return: void
  */
 void free_dog(dog_t *d)
 {
-    if (d != NULL)
-    {
-        free(d->name);
-        free(d->owner);
-        free(d);
-    }
+	if (d != NULL)
+	{
+	free(d->name);
+	free(d->owner);
+	free(d);
+	}
 }
