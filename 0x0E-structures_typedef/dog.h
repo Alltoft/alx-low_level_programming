bf319655b2e5d03262309dbeb3a742633f562d4a
Emran <ouakouriimran@gmail.com>
2023-08-14 15:36:37 +0100

    god structer

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,16 @@
+#ifndef MAIN_H
+#define MAIN_H
+/**
+ * struct dog - givs dog info
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: dog owner
+ * Return: void
+ */
 struct dog
 {
 	char *name;
 	float age;
 	char *owner;
 };
+#endif
