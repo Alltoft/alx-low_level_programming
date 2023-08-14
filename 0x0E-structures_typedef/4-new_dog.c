bbc67fa48e8d3231d95bbf56a897a4843e029f11
Emran <ouakouriimran@gmail.com>
2023-08-14 20:07:14 +0100

    putting

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,16 @@
+#include "dog.h"
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *new_dog;
+
+	new_dog = malloc(sizeof(dog_t));
+	if (new_dog == NULL)
+		return (NULL);
+	new_dog->name = strdup(name);
+	new_dog->age = age;
+	new_dog->owner = strdup(owner);
+	return (new_dog);
+}
