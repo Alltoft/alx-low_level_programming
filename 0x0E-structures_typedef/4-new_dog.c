ccca6766eb6cb613108a2065c7fe8fbb25a076ac
Emran <ouakouriimran@gmail.com>
2023-08-14 20:58:19 +0100

    fish smell

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -37,27 +37,37 @@ dog_t *new_dog(char *name, float age, char *owner)
 	}
 	return (new_dog);
 }*/
+/**
+ * new_dog - creates a new dog.
+ * @name: name of the dog.
+ * @age: age of the dog.
+ * @owner: owner of the dog.
+ *
+ * Return: pointer to the new dog.
+*/
 dog_t *new_dog(char *name, float age, char *owner)
 {
-        dog_t *new_dog;
+	dog_t *dog = malloc(sizeof(dog_t));
+		if (dog == NULL)
+			return (NULL);
 
-        new_dog = malloc(sizeof(dog_t));
-        if (new_dog == NULL)
-                return (NULL);
-        new_dog->name = strdup(name);
-        if (new_dog->name == NULL)
-        {
-                free(new_dog);
-                return (NULL);
-        }
-        new_dog->age = age;
-        new_dog->owner = strdup(owner);
-        if (new_dog->owner == NULL)
-        {
-                free(new_dog->name);
-                free(new_dog);
-                return (NULL);
-        }
-        return (new_dog);
+		dog->name = (char *) malloc(strlen(name) + 1);
+
+		if (dog->name == NULL)
+		{
+			free(dog);
+			return (NULL);
+		}
+	strcpy(dog->name, name);
+	dog->age = age;
+	dog->owner = (char *) malloc(strlen(owner) + 1);
+
+	if (dog->owner == NULL)
+	{
+		free(dog->name);
+		free(dog);
+		return (NULL);
+	}
+	strcpy(dog->owner, owner);
+		return (dog);
 }
-
