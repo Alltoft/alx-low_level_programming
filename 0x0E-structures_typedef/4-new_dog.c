caa3bbecdb2090b89f17bba3934b5e76fa5f7f19
Emran <ouakouriimran@gmail.com>
2023-08-14 20:10:47 +0100

    putting

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -2,6 +2,13 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
+/**
+ * *new_dog - action a new dog
+ * @name: dog name
+ * @age: dog age
+ * @owner: dog owner
+ *Return: new_dog
+*/
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *new_dog;
