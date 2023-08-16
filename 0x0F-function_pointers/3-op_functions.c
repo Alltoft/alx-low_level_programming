d51ae526f64e42820d66a68530f9b65fe0dc1157
Emran <ouakouriimran@gmail.com>
2023-08-16 23:29:56 +0100

    not complete yet

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,54 @@
+#include "3-calc.h"
+#include "stdio.h"
+#include "stdlib.h"
+#include "string.h"
+/**
+ * op_add - the remainder of the division of a by b
+ * @a: first number
+ * @b: sec number
+ * Return: the Result
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+/**
+ * op_sub - the remainder of the division of a by b
+ * @a: first number
+ * @b: sec number
+ * Return: the Result
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+/**
+ * op_mul - the remainder of the division of a by b
+ * @a: first number
+ * @b: sec number
+ * Return: the Result
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+/**
+ * op_div - the remainder of the division of a by b
+ * @a: first number
+ * @b: sec number
+ * Return: the Result
+ */
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+/**
+ * op_mod - the remainder of the division of a by b
+ * @a: first number
+ * @b: sec number
+ * Return: the Result
+ */
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
