da5147e687715d265c190e265bf5ab624fbe25bb
Emran <ouakouriimran@gmail.com>
2023-08-29 00:36:06 +0100

    deldeting

---
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * pop_listint - for being rude
+ * @head: the master of them
+ * Return: value of delted node
+ */
+
+int pop_listint(listint_t **head)
+{
+	listint_t *t;
+	int n;
+
+	if (!head)
+		return (0);
+	n = (*head)->n;
+	t = *head;
+	*head = (*head)->next;
+	free(t);
+return (n);
+}
