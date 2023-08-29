bf220c5b6b0e489498460d4c216d9c6d18c650de
Emran <ouakouriimran@gmail.com>
2023-08-29 20:24:03 +0100

    guys

---
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -0,0 +1,37 @@
+#include "lists.h"
+
+/**
+ * *insert_nodeint_at_index - count number of elements
+ * @head: the way of the sword
+ * @index: git push
+ * Return: number of soliders
+ */
+
+listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
+{
+
+
+	listint_t *nd = malloc(sizeof(listint_t));
+	listint_t *t = *head;
+	unsigned int i;
+
+	if (!head || !nd)
+		return (NULL);
+	
+	nd->n = n;
+
+	if (!idx)
+	{
+		nd->next = *head;
+		*head = nd;
+		return (nd);
+	}
+	
+	for (i = 0; t != NULL && i < idx - 1; i++)
+		t = t->next;
+	if (t == NULL)
+		return (NULL);
+	nd->next = t->next;
+	t->next = nd;
+	return (nd);
+}
