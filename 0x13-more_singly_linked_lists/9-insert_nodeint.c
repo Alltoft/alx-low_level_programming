20d9b3f8e32525be1ab3a8de4fbd39944c089b92
Emran <ouakouriimran@gmail.com>
2023-08-29 20:26:46 +0100

    walo

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -3,21 +3,20 @@
 /**
  * *insert_nodeint_at_index - count number of elements
  * @head: the way of the sword
- * @index: git push
+ * @idx: git push
+ * @n: the cannons
  * Return: number of soliders
  */
 
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
-
-
 	listint_t *nd = malloc(sizeof(listint_t));
 	listint_t *t = *head;
 	unsigned int i;
 
 	if (!head || !nd)
 		return (NULL);
-	
+
 	nd->n = n;
 
 	if (!idx)
@@ -26,7 +25,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		*head = nd;
 		return (nd);
 	}
-	
+
 	for (i = 0; t != NULL && i < idx - 1; i++)
 		t = t->next;
 	if (t == NULL)
