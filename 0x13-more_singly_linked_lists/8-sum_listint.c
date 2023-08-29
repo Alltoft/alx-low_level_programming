9aec86fce3f3749747f3fbbc72d83538a87b7886
Emran <ouakouriimran@gmail.com>
2023-08-29 19:15:32 +0100

    number

--- 0x13-more_singly_linked_lists/8-sum_listint.c
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -1,1 +1,19 @@
-#include <>
+#include "lists.h"
+
+/**
+ * sum_listint - count number of elements
+ * @head: the way of sword
+ * Return: number of soliders
+ */
+
+int sum_listint(listint_t *head)
+{
+	int count = 0;
+
+	while (head)
+	{
+		count += head->n;
+		head = head->next;
+	}
+	return (count);
+}
