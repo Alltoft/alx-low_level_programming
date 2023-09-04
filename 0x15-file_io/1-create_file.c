c945a19e8dc10fa51dc2c0e1e85b5265cda2200f
Emran <ouakouriimran@gmail.com>
2023-09-05 00:44:26 +0100

    chihaja

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -11,12 +11,17 @@ int create_file(const char *filename, char *text_content)
 {
 	FILE *fp;
 
-	fp = fopen(filename, "w+");
-	if (fp == NULL)
+	if (text_content == NULL)
+	{
+		fp = fopen(filename, "w+");
+		if (fp == NULL)
+			return (-1);
+
+		fputs(text_content, fp);
+
+		fclose(fp);
+		return (1);
+	}
+	else
 		return (-1);
-
-	fputs(text_content, fp);
-
-	fclose(fp);
-	return (1);
 }
