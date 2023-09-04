1ab16109d09296b07f0a23248ec326078634cace
Emran <ouakouriimran@gmail.com>
2023-09-05 00:36:37 +0100

    not

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -10,17 +10,13 @@
 int create_file(const char *filename, char *text_content)
 {
 	FILE *fp;
-	int w;
 
 	fp = fopen(filename, "w+");
 	if (fp == NULL)
 		return (-1);
 
-	w = strlen(text_content);
-
 	fputs(text_content, fp);
 
-	free(buf);
 	fclose(fp);
 	return (1);
 }
