015803e2e0b469cc561fe40eb13c85c29f66a1c4
Emran <ouakouriimran@gmail.com>
2023-09-05 01:37:00 +0100

    guess

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -1,20 +1,20 @@
 #include "main.h"
 
 /**
- * create_file - creats a file without perm
+ * append_text_to_file - creats a file without perm
  * @filename: the filename
  * @text_content: u who tell me
  * Return: 1 if succeed -1 if not
  */
 
-int create_file(const char *filename, char *text_content)
+int append_text_to_file(const char *filename, char *text_content)
 {
 	int fd;
 
 	if (filename == NULL)
 		return (-1);
 
-	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
+	fd = open(filename, O_WRONLY | O_APPEND);
 	if (fd == -1)
 		return (-1);
 
