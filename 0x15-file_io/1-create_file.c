8a67e0fb242b8821f8c70c9e2b2ef600472b4534
Emran <ouakouriimran@gmail.com>
2023-09-05 01:05:24 +0100

    maybe

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -19,6 +19,8 @@ int create_file(const char *filename, char *text_content)
 		return (-1);
 
 	fputs(text_content, fp);
+	if (fputs(text_content, fp) == EOF)
+		return (-1);
 
 	fclose(fp);
 	return (1);
