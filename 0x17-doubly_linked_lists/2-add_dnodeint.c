c2c1e92c11e2fbb734497fcf1036f25f73270e17
Emran <ouakouriimran@gmail.com>
2023-10-12 17:24:22 +0100

    Dancing is what to do

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -1,7 +1,15 @@
 #include "lists.h"
+/**
+ * add_dnodeint - adds a new node at the beginning of a doubly linked list
+ * @head: pointer to head node
+ * @n: value to store in new node
+ *
+ * Return: address of new node, or NULL if it failed
+ */
 dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
 	dlistint_t *new;
+
 	new = malloc(sizeof(dlistint_t));
 	if (new == NULL)
 		return (NULL);
@@ -12,5 +20,5 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 		(*head)->prev = new;
 	*head = new;
 	free(new);
-	return(new);
+	return (new);
 }
