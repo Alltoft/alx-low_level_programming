39964ad7c1f648240275f4974d866b3f17b7405f
Emran <ouakouriimran@gmail.com>
2023-11-03 00:05:10 +0100

    sir ga3 pochi

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,17 @@
+#include "hash_tables.h"
+
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	hash_table_t *new_table;
+
+	new_table = malloc(sizeof(hash_table_t));
+	if (!new_table)
+		return (NULL);
+
+	new_table->size = size;
+	new_table->array = malloc(sizeof(hash_node_t));
+	if (!new_table->array)
+		return (NULL);
+
+	return (new_table);
+}
---
+++ 0x1A-hash_tables/README.md
@@ -0,0 +1,1 @@
+the Hash Table emplimentations
